61ee4ca95e7cb344540d01a3d172302c4a66dff5
Ainura Usenova <ainura8921new@gmail.com>
2022-05-09 14:59:15 +0600

    first-commit

---
+++ src/fire.js
@@ -0,0 +1,22 @@
+
+import firebase from 'firebase/compat/app'  
+import 'firebase/compat/app'  
+import 'firebase/compat/auth'  
+  
+  
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_lEVy7Cq2U61T_Ir50In8kxYYe1OFlpo",
+    authDomain: "online-school-ae761.firebaseapp.com",
+    projectId: "online-school-ae761",
+    storageBucket: "online-school-ae761.appspot.com",
+    messagingSenderId: "529888702922",
+    appId: "1:529888702922:web:e04da9a8a237fcb7d09f33"
+  };
+
+  
+  
+const fire = firebase.initializeApp(firebaseConfig)  
+  
+  
+export default fire;
